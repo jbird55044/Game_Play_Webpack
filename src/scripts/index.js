54469cf7aab028e4b05d879d678ef7ff137d9bb2
James Bird <jbird@JamesDBird.com>
2020-10-15 13:06:39 -0500

    first commit, laying groundwork

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,40 @@
-import '../styles/index.scss';
+console.log('webpack starterkit');
 
-if (process.env.NODE_ENV === 'development') {
-  require('../index.html');
+var myGamePiece;
+
+function startGame() {
+    myGamePiece = new component(30, 20, "red", 580, 20);
+    myGameArea.start();
 }
 
-console.log('webpack starterkit');
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 600;
+        this.canvas.height = 400;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    this.update = function(){
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.x -= 5;
+    myGamePiece.update();
+};
